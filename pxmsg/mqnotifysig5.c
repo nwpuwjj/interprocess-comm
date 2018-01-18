555a303f868bbe419b61b58c7cb3a4be3bb9eeda
wangjj <wangjj@qtec.cn>
2018-01-17 22:52:57 -0800

    posix message queue (mq_notify 5)  select

---
+++ pxmsg/mqnotifysig5.c
@@ -0,0 +1,67 @@
+#include "unpipc.h"
+
+int pipefd[2];
+static void sig_usr1(int);
+
+int main(int argc, char ** argv)
+{
+    int nfds;
+    char c;
+    fd_set rset;
+    mqd_t mqd;
+    void *buff;
+    ssize_t n;
+    
+    struct mq_attr attr;
+    struct sigevent sigev;
+
+    if(argc != 2)
+    {
+        DEBUG_PRINTF_RED("usage: mqnotifysig5 <name>");
+        exit(0);
+    }
+
+    /* open queue, get attributes, allocate read buffer */
+    mqd=mq_open(argv[1],O_RDONLY | O_NONBLOCK);
+    mq_getattr(mqd,&attr);
+    buff=malloc(attr.mq_msgsize);
+
+    pipe(pipefd);
+
+    /*establish signal handler, enable notification*/
+    signal(SIGUSR1,sig_usr1);
+    sigev.sigev_notify=SIGEV_SIGNAL;
+    sigev.sigev_signo=SIGUSR1;
+    mq_notify(mqd,&sigev);
+
+    FD_ZERO(&rset);
+    for(;;)
+    {
+        FD_SET(pipefd[0],&rset);
+        nfds=select(pipefd[0]+1,&rset,NULL,NULL,NULL);
+
+        if(FD_ISSET(pipefd[0],&rset))
+        {
+            read(pipefd[0],&c,1);
+            mq_notify(mqd,&sigev); /*reregister first */
+            while( (n=mq_receive(mqd,buff,attr.mq_msgsize,NULL)) >=0)
+            {
+                DEBUG_PRINTF("read %ld bytes \n",(long)n);
+            }
+
+            if(errno !=EAGAIN)
+            {
+                DEBUG_PRINTF_RED("mq_receive error");
+                exit(0);
+            }
+        }
+    }
+
+    exit(0);
+}
+
+static void sig_usr1(int signo)
+{
+    write(pipefd[1],"",1); /*one byte of 0 */
+    return;
+}
